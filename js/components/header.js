5d0e00fa8b2bf95ad72a393cb18a23327b3cf676
Rizky Rosyid Hidayat <rizky.hidayat@students.amikom.ac.id>
2020-05-13 18:56:39 +0700

    revisi header

--- js/components/header.js
+++ js/components/header.js
@@ -1,3 +1,76 @@
+Vue.component('navbar-section', {
+	template: `
+		<div class="navbar">
+			<div class="container">					
+				<div class="navbar-nav">
+					<a href="">
+						<img 
+							src="./img/logo.png" 
+							class="logo"
+							alt="logo bit energy" 
+						/>
+					</a>
+					<div class="d-lg-none">
+						<img 
+							src="./img/icon/menu.svg"
+							alt="icon menu" 
+							height="30"
+							@click="$emit('active', true)"
+						/>
+					</div>
+					<div class="d-sm-none d-flex">
+						<a 
+							v-for="item in navbarNav" :key="item"
+							@click="jumping = item" 
+							:href="'#'+item" 
+							class="nav-link pa-2 text-uppercase"
+							:class="{'active': item === jumping?true:false}">
+							{{ item }}
+						</a>
+						<div class="dropdown pa-2 text-uppercase">
+							<div class="d-flex">
+								<div class="mr-1">other</div>
+								<img 
+									src="./img/icon/chevron-down.svg"
+									alt="icon chevron-down" 
+									class="rotate90"
+								/>
+							</div>
+							<ul class="dropdown-menu">
+								<li 
+									v-for="item in otherNav" :key="item.text"
+									@click="jumping = item">
+									<a 
+										class="nav-link text-uppercase"
+										:href="item.value">
+										{{ item.text }}
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	`,
+	data: () => ({
+		navbarNav: [
+			'overview', 
+			'features', 
+			'why?', 
+			'application', 
+			'project'
+		],
+		otherNav: [
+			{ text: 'consultation', value: '#consultation' }, 
+			{ text: 'our team', value: '#our_team' }, 
+			{ text: 'testimonial', value: '#testimonial' }, 			
+			{ text: 'our partners', value: '#our_partners' }
+		],
+		jumping: 'overview'
+	})
+})
+
 Vue.component('header-section', {
 	template: `
 		<header>
@@ -82,7 +155,8 @@ Vue.component('header-section', {
 						</div>
 					</div>
 				</div>
-			</div>			
+			</div>	
+			<navbar-section></navbar-section>
 		</header>
 	`,
 	data: () => ({
@@ -107,62 +181,3 @@ Vue.component('header-section', {
 		jumping: 'overview'
 	})
 })
-
-// <div class="navbar">
-// 				<div class="container">					
-// 					<div class="navbar-nav">
-// 						<div class="d-lg-none">
-// 							<img 
-// 								src="./img/icon/menu.svg"
-// 								alt="icon menu" 
-// 								height="30"
-// 								@click="$emit('active', true)"
-// 							/>
-// 						</div>
-// 						<div class="d-sm-none d-flex">
-// 							<a 
-// 								v-for="item in navbarNav" :key="item"
-// 								:href="'#'+item"
-// 								@click="jumping = item" 
-// 								class="nav-link pa-2 text-uppercase"
-// 								:class="{'active': item === jumping?true:false}">
-// 								{{ item }}
-// 							</a>
-// 							<div class="dropdown pa-2 text-uppercase">
-// 								<div class="d-flex">
-// 									<div class="mr-1">other</div>
-// 									<img 
-// 										src="./img/icon/chevron-down.svg"
-// 										alt="icon chevron-down" 
-// 										class="rotate90"
-// 									/>
-// 								</div>
-// 								<ul class="dropdown-menu">
-// 									<li 
-// 										v-for="item in otherNav" :key="item.text"
-// 										@click="jumping = item" >
-// 										<a 
-// 											class="nav-link text-uppercase"
-// 											:href="item.value">
-// 											{{ item.text }}
-// 										</a>
-// 									</li>
-// 								</ul>
-// 							</div>
-// 						</div>
-// 						<div class="form-control rounded search">
-// 							<div class="form-control-icon left">
-// 								<img 
-// 									src="./img/icon/magnify.svg"
-// 									alt="icon magnify" 
-// 								/>
-// 							</div>
-// 							<input 
-// 								type="text" 
-// 								name="search" 
-// 								placeholder="Find Something"
-// 							/>
-// 						</div>
-// 					</div>
-// 				</div>
-// 			</div>
